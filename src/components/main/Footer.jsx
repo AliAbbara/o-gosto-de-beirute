df6454745cffc5fcca6a2fead460e853b1f718cc
AliAbbara <abbara21@gmail.com>
2023-04-07 21:54:36 -0300

    fixed footer responsivesness

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -9,38 +9,48 @@ function Footer() {
   return (
     <ContainerCard className='mt-6 mb-20 sm:mb-auto flex flex-col text-center'>
       {/* -----------Logo and menu div------------- */}
-      <div className='flex flex-wrap justify-evenly p-2'>
+      <div className='flex flex-col sm:flex-row flex-wrap justify-evenly items-center p-2'>
         {/* Logo */}
-        <img src={logo} alt='full logo' className='w-44 h-auto' />
+        <img
+          src={logo}
+          alt='full logo'
+          className='w-32 h-32 sm:w-44 sm:h-auto'
+        />
         {/* Social Media */}
-        <div className='flex flex-col items-center justify-evenly'>
+        <div className='flex flex-col items-center'>
           <p className='text-3xl mb-2'>Redes</p>
-          <RedLink
-            target='_blank'
-            to='https://www.instagram.com/ogostodebeirute/'>
-            <BsInstagram />
-          </RedLink>
-          <RedLink to='/contact'>
-            <BsWhatsapp />
-          </RedLink>
-          <RedLink
-            target='_blank'
-            to='https://www.ifood.com.br/delivery/barretos-sp/o-gosto-centro/71057262-5b24-4e15-8e0c-0a7f54dc008d?UTM_Medium=share'>
-            <SiIfood />
-          </RedLink>
-        </div>
-        {/* Links */}
-        <div className='flex flex-col'>
-          <p className='text-3xl mb-2'>Cardapio</p>
-          <RedLink to='/menu/sandwiches'>Lanches</RedLink>
-          <RedLink to='/menu/porcoes'>Salgados & Porcoes</RedLink>
-          <RedLink to='/menu/drinks'>Bebidas</RedLink>
+          <div className=' flex sm:flex-col'>
+            <RedLink
+              className='mb-1'
+              target='_blank'
+              to='https://www.instagram.com/ogostodebeirute/'>
+              <BsInstagram />
+            </RedLink>
+            <RedLink className='mb-1' target='_blank' to='/contact'>
+              <BsWhatsapp />
+            </RedLink>
+            <RedLink
+              className='mb-1'
+              target='_blank'
+              to='https://www.ifood.com.br/delivery/barretos-sp/o-gosto-centro/71057262-5b24-4e15-8e0c-0a7f54dc008d?UTM_Medium=share'>
+              <SiIfood />
+            </RedLink>
+          </div>
         </div>
         {/* Menu */}
-        <div className='flex flex-col justify-evenly'>
+        <div className='flex flex-col'>
           <p className='text-3xl mb-2'>Paginas</p>
-          <RedLink to='/contact'>Contact</RedLink>
-          <RedLink to='/about-us'>About Us</RedLink>
+          <div className=' flex sm:flex-col'>
+            <RedLink className='sm:mb-1 py-0' to='/menu/sandwiches'>
+              Cardapio
+            </RedLink>
+            <RedLink className='sm:mb-1 py-0' to='/contact'>
+              Contato
+            </RedLink>
+            <RedLink className='py-0' to='/about-us'>
+              Sobre
+            </RedLink>
+          </div>
         </div>
       </div>
       {/* -----------Rights div------------- */}
