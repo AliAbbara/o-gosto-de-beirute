04735f361327bdf84a5a62ff371db12b9dbfeef8
AliAbbara <abbara21@gmail.com>
2023-04-09 16:30:11 -0300

    Added local background img & local font

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './assets/fonts/Cuprum-Regular.ttf'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
