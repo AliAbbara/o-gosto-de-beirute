e49452def326381d4897994bd37d750204e37014
AliAbbara <abbara21@gmail.com>
2023-04-13 09:08:42 -0300

    Fixed RingCard ring positioning

--- src/components/cards/RingCard.jsx
+++ src/components/cards/RingCard.jsx
@@ -7,7 +7,7 @@ function RingCard({ children, className, title }) {
         'mb-4 mt-20 p-4 pt-20 relative items-center text-lg text-white rounded-lg bg-red-700 border-2 border-yellow-300 shadow-2xl shadow-black ' +
         className
       }>
-      <div className='absolute -top-16 -left-1.5 w-32 flex justify-center items-center'>
+      <div className='absolute w-ring flex justify-center items-center'>
         <img src={circleLogo} alt='logo' />
         <div className='w-fit absolute text-3xl rounded-lg backdrop-blur-md backdrop-contrast-125'>
           <p>{title}</p>
